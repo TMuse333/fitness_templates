494bd4ec68cc99b13f7802a547964eb9d1368bc7
Thomas Musial <th896493@dal.ca>
2024-03-07 17:31:56 -0400

    the center is cheesing

---
+++ .history/src/components/optionDisplay/optionDisplay_20240307163520.jsx
@@ -0,0 +1,132 @@
+import React, { useState,useEffect } from 'react';
+import './optionDisplay.css';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+
+
+
+import abu6 from '../../media/aboubacar-6.jpg';
+import abu5 from '../../media/aboubacar-5-fire.png';
+import striker from '../../media/quantum-striker-pose.png';
+import vegeta from '../../media/vegeta-battle.png';
+
+const ProductCarousel = () => {
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [buttonClicked, setButtonClicked] = useState(null);
+
+  const [hovered, setIsHovered ] = useState(null)
+
+  useEffect(() => {
+    // Empty useEffect
+  }, []);
+  
+
+  const handleMouseLeave = () => {
+    setIsHovered(null)
+  }
+
+  const products = [
+    {
+      image: striker,
+      name: 'the striker',
+      description: 'the quantum realm is strong',
+    },
+    {
+      image: abu5,
+      name: 'Abu fire',
+      description: 'Aboubacar flexing with the fire',
+    },
+    {
+      image: abu6,
+      name: 'Abu flex',
+      description: 'Flexing at glenbourne',
+    },
+    {
+      image: vegeta,
+      name: 'Vegeta',
+      description: 'Be a proud warrior like Vegeta',
+    },
+  ];
+
+  const positions = Array.from({ length: products.length }, (_, index) => {
+  
+  
+    return {
+      left: `${index * 50}%`,
+      transform: `translateX(-50%)`,
+     
+      transition: 'transform 0.3s ease, border 0.3s ease, box-shadow 0.3s ease',
+    };
+  });
+
+  const handleMouseEnter = (index) => {
+    setIsHovered((index) )
+  }
+  
+
+  const handlePrevClick = () => {
+    setCurrentPosition((prevPosition) =>
+      prevPosition === 0 ? positions.length - 1 : prevPosition - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentPosition((prevPosition) => (prevPosition + 1) % positions.length);
+  };
+
+ 
+  
+
+
+  return (
+    <div className="product-carousel-container">
+      <button className="carousel-button" onClick={handlePrevClick}>
+      <FiChevronLeft />
+      </button>
+      <button className="carousel-button" onClick={handleNextClick}>
+      <FiChevronRight />
+      </button>
+      <div className="products-row">
+        {products.map((product, index) => (
+          <div
+            key={index}
+            className="product"
+            style={{
+                left: positions[(currentPosition + index) % positions.length].left,
+                transform: `${
+                    positions[(currentPosition + index) % positions.length].transform
+                  } ${positions[(currentPosition + index) % positions.length].left ===
+                  '50%' ? 'scale(1.2)' : ''}`,
+              
+                transition: positions[(currentPosition + index) % positions.length].transition + ', left 0.5s ease, transform 0.5s ease',
+              }}
+           
+           
+          >
+            <img 
+            src={product.image}
+             alt={`Product ${index + 1}`}
+            key={index}
+            // onMouseEnter={()=>handleMouseEnter((currentPosition + 1) % positions.length)}
+            
+
+                
+
+            onMouseLeave={()=>handleMouseLeave()} />
+            <div
+              style={{
+                opacity: positions[(currentPosition + index) % positions.length].left !== '50%' ? '0' : '1',
+                transition: 'opacity 0.5s ease 0.5s',
+              }}
+            >
+             
+            </div>
+          </div>
+        ))}
+          <FiChevronRight />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCarousel;
---
+++ .history/src/pages/program_20240307172612.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Herobanner2 from "../components/herobanner2/herobanner2";
+import dunk from '../media/best_dunk.mp4';
+import Preview from "../components/preview/preview";
+import HydroList from "../components/hydrolist/hydrolist";
+import { designAdvantages2 } from "../data/data";
+import Navbar3 from "../components/navbar3/navbar3";
+import Footer from "../components/footer/footer";
+import Footer2 from "../components/footer2/footer2";
+import Navbar from "../components/navbar/navbar";
+import Closer from "../components/closer/closer";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import ProductCarousel from "../components/optionDisplay/optionDisplay";
+
+const Program = () => {
+
+    const links = [
+        {
+            dest:'/',
+            name:'Home'
+        },
+        {
+            dest:'/about',
+            name:'About'
+        },
+        {
+            dest:'/contact',
+            name:'Contact'
+        }
+    ]
+
+
+    return (
+        <div className="program-container">
+            <Helme
+                        <Helmet>
+                <title>Muhammad Bensalim Training</title>
+                <meta name="Training Overview" content="This page is an overview of Muhammad Bensalims training services,
+                it describes his philosophies can methods for acheive the best fitness results possible. It also answers common
+                training questions like what equipment and mentality is needed ect." />
+              
+            </Helmet>
+
+            <Navbar
+            links={links}/>
+<Herobanner2/>
+
+<video autoPlay muted loop className='program-video'>
+        <source src='' type="video/mp4" />
+      </video>
+      <Preview/>
+
+<div className="questions-list">
+
+<h1>Questions</h1>
+
+
+      <HydroList
+      text={designAdvantages2}
+    absolute={true}
+      />
+
+</div>
+
+<ProductCarousel/>
+
+<Closer/>
+
+<Footer
+           links={links}/>
+          
+        </div>
+
+       
+    )
+
+}
+
+export default Program
