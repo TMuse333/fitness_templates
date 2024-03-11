4afd1712fa9d1c53f70af79e61310bf8fcd9ac60
Thomas Musial <th896493@dal.ca>
2024-03-11 17:39:06 -0300

    more computations

---
+++ .history/src/components/tracker/tracker_20240311173637.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useWorkout } from "../../context/context";
+import "./tracker.css";
+
+const Tracker = () => {
+  const {
+    selectedDate,
+    handleDateChange,
+    handleWeekSelection,
+    handleDateSelection,
+    filteredWorkouts,
+    calculateProgress,
+    handleProgressSelection,
+    weeklyProgressSelected
+  } = useWorkout();
+
+  return (
+    <div className="tracker-container">
+      <p>Select a date</p>
+      <div className="tracker-buttons">
+        <button onClick={handleDateSelection}>Date</button>
+        <button onClick={handleWeekSelection}>Week</button>
+        <button onClick={handleProgressSelection}>Progress</button>
+      </div>
+
+      <form className="tracker-form">
+        <DatePicker
+          placeholderText="Search for a date"
+          selected={selectedDate}
+          onChange={handleDateChange}
+          className="date-picker"
+        />
+      </form>
+
+      {filteredWorkouts.length > 0 &&   !weeklyProgressSelected ? (
+        <div className="workout-boxes">
+          {/* Render workout plans for the selected date or week */}
+          {filteredWorkouts.map((workout, index) => (
+            <div key={index} className="exercise-date">
+              <strong>Date:</strong> {workout.date.toDateString()}
+              {workout.exercises.map((exercise, exerciseIndex) => (
+                <div key={exerciseIndex} className="exercise-box">
+                  <strong>Exercise:</strong> {exercise.name}
+                  {exercise.sets.map((set, setIndex) => (
+                    <div key={setIndex}>
+                      <strong>Set {setIndex + 1}:</strong> Weight: {set.weight}, Reps: {set.reps}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No workout done on this day</p>
+      )}
+
+      {/* Render progress data */}
+      {calculateProgress().length > 0 &&(
+        <div className="exercise-progress-container">
+          <h2>Progress Data</h2>
+          {calculateProgress().map((progress, progressIndex) => (
+            <div key={progressIndex} className="progress-exercise">
+              <strong>Exercise:</strong> {progress.name}
+              {progress.progressSets.map((progressSet, setIndex) => (
+                <div key={setIndex}>
+                  <strong>From Workout {setIndex} to Workout {setIndex + 1}:</strong> 
+                  Weight Diff: {progressSet.weightDiff}, Reps Diff: {progressSet.repsDiff}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tracker;
---
+++ .history/src/context/context_20240311163816.jsx
@@ -0,0 +1,175 @@
+// WorkoutContext.js
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { workoutData } from '../data/data';
+
+const WorkoutContext = createContext();
+
+export const WorkoutProvider = ({ children }) => {
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [workouts, setWorkouts] = useState([]);
+    const [selectedWeek, setSelectedWeek] = useState(null);
+  
+    const [weekSelected, setWeekSelected] = useState(false);
+    const [dateSelected, setDateSelected] = useState(false);
+
+    const [weeklyProgressSelected, setWeeklyProgressSelected] =
+    useState(false)
+
+    const handleProgressSelection = () => {
+        setWeeklyProgressSelected(true)
+    
+        calculateProgress()
+    }
+  
+    const exerciseMap = new Map();
+  
+    useEffect(() => {
+      // Fetch your workout data here
+      // For now, using the provided workoutData as an example
+      setWorkouts(workoutData);
+    }, []);
+  
+    const handleDateChange = (date) => {
+      setSelectedDate(date);
+      // Perform any additional logic based on the selected date
+    };
+  
+    const handleWeekSelection = () => {
+      setWeekSelected(true);
+      setDateSelected(false);
+      setSelectedWeek(getWeekNumber(selectedDate));
+    };
+  
+    const handleDateSelection = () => {
+      setDateSelected(true);
+      setWeekSelected(false);
+    };
+  
+    const getWeekNumber = (date) => {
+      // Calculate the ISO week number
+      const currentDate = new Date(date);
+      currentDate.setHours(0, 0, 0, 0);
+      currentDate.setDate(currentDate.getDate() + 4 - (currentDate.getDay() || 7));
+      const yearStart = new Date(currentDate.getFullYear(), 0, 1);
+      const weekNumber = Math.ceil(((currentDate - yearStart) / 86400000 + 1) / 7);
+  
+      return weekNumber;
+    };
+  
+    // Filter workouts based on the selected date or week
+    const filteredWorkouts = workouts.filter((workout) => {
+      if (dateSelected) {
+        return workout.date && workout.date.toDateString() === selectedDate?.toDateString();
+      } else if (weekSelected) {
+        return getWeekNumber(workout.date) === selectedWeek;
+      }
+      return false;
+    });
+
+
+    const calculateProgress = () => {
+        if (weekSelected) {
+          // Iterate through workouts in the selected week
+          const sortedWorkouts = filteredWorkouts.sort((a, b) => a.date - b.date);
+      
+          // Calculate progress for each exercise
+          const progressData = [];
+      
+          sortedWorkouts.forEach((workout) => {
+            workout.exercises.forEach((exercise) => {
+              const { name } = exercise;
+      
+              // Check if the exercise already exists in progressData
+              const existingExercise = progressData.find((item) => item.name === name);
+      
+              if (existingExercise) {
+                // If the exercise exists, add the current workout data to it
+                existingExercise.workouts.push({
+                  date: workout.date,
+                  sets: exercise.sets,
+                });
+              } else {
+                // If the exercise doesn't exist, create a new entry in progressData
+                progressData.push({
+                  name,
+                  workouts: [
+                    {
+                      date: workout.date,
+                      sets: exercise.sets,
+                    },
+                  ],
+                });
+              }
+            });
+          });
+
+
+      
+          // Log the workout data for each exercise
+          progressData.forEach((exercise) => {
+            console.log(`Exercise Name: ${exercise.name}`);
+      
+            for (let i = 0; i < exercise.workouts.length ; i++) {
+
+                for(let j = 0; j < exercise.workouts[i].sets.length -1; j++){
+                    const diff = exercise.workouts[i].sets[j + 1].weight -
+                    exercise.workouts[i].sets[j]
+                    console.log('weight difference is ',diff)
+                }
+               
+               
+
+            }
+          });
+
+
+        //   console.log('first progress',progressData[0].workouts[0].sets[0].weight)
+       
+          
+          console.log('Progress Data:', progressData);
+      
+          return progressData;
+        }
+      
+        return [];
+      };
+      
+      
+      
+      
+
+
+
+    const value = {
+        filteredWorkouts,
+        handleWeekSelection,
+        handleDateSelection,
+        handleDateChange,
+        selectedDate,
+        handleProgressSelection,
+        weeklyProgressSelected,
+        calculateProgress
+    }
+  
+
+  // Additional functions or calculations related to workout data can be added here
+
+  return (
+    <WorkoutContext.Provider
+      value={value}
+    >
+      {children}
+    </WorkoutContext.Provider>
+  );
+};
+
+export const useWorkout = () => {
+  const context = useContext(WorkoutContext);
+
+  if (!context) {
+    throw new Error('useWorkout must be used within a WorkoutProvider');
+  }
+
+  return context;
+};
